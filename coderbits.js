fde958424af5730b4dff50f05aeacaf69e1a255b
Rodrigo Medeiros <rodrigo.mangue@gmail.com>
2015-09-14 00:08:13 -0300

    Adjustments to conform with eslint.

--- coderbits.js
+++ coderbits.js
@@ -5,18 +5,26 @@ var coderbits = function (user, callback) {
 
   var url = config.url;
 
-  if (arguments.length < 2) throw new Error("An user string (or an options object) and a callback are required.");
-  if (typeof callback !== 'function') throw new Error(typeof callback + " is not a valid callback.");
+  if (arguments.length < 2) {
+    throw new Error("An user string (or an options object) and a callback are required.");
+  }
+  if (typeof callback !== 'function') {
+    throw new Error(typeof callback + " is not a valid callback.");
+  }
   if (typeof user === 'string') {
     url += user + '.json';
-  } else if (typeof user === 'object') {
-    if (user.username && typeof user.username === 'string') {
-      url += user.username + '.json';
-      if (user.account && typeof user.account === 'string') {
-        url += '?account=' + user.account;
+  } else {
+    if (typeof user === 'object') {
+      if (user.username && typeof user.username === 'string') {
+        url += user.username + '.json';
+        if (user.account && typeof user.account === 'string') {
+          url += '?account=' + user.account;
+        }
       }
+    } else {
+      throw new Error(typeof user + " is not a valid user string neither an options object.");
     }
-  } else throw new Error(typeof user + " is not a valid user string neither an options object.");
+  }
 
   https.get(url, function (response) {
     var body = '';
